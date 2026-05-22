4fbc359decde11895aa9ad61b6a859422149d96f
Rajendra Arkara <rajendraarkara@gmail.com>
2026-05-23 00:30:14 +0700

    update

--- routes/events.go
+++ routes/events.go
@@ -66,3 +66,33 @@ func createEvents(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "event created!", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"Message": "Could not parse event ID",
+			"Error":   err.Error(),
+		})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"Message": "Could not fetch events. Try again later",
+			"Error":   err.Error(),
+		})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"Message": "could not parse the data",
+			"error":   err.Error(),
+		})
+		return
+	}
+}
--- routes/routes.go
+++ routes/routes.go
@@ -6,4 +6,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/event", getEvents)
 	server.GET("/event/:id", getEvent)
 	server.POST("/event", createEvents)
+	server.PUT("/event/:id")
 }
