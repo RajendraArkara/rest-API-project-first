736331e90a14f70b33406bff61af86bad6e2e7a6
Rajendra Arkara <rajendraarkara@gmail.com>
2026-07-01 13:39:55 +0700

    update juni akhir

--- routes/events.go
+++ routes/events.go
@@ -42,6 +42,15 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvents(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{
+			"Message": "Not Authorize",
+		})
+		return
+	}
+
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
 
